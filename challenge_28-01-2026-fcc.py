cbe3b48405c2c347a8cf34df093a2e025f16b90a
Mayra Silva <mayraeduarda2002@gmail.com>
2026-01-28 13:44:39 -0300

    challenge 1 - step 0.1

--- challenge_28-01-2026-fcc.py
+++ challenge_28-01-2026-fcc.py
@@ -10,10 +10,14 @@ Retain the original order of the items in the arrays.
 Tests
 
 1. flatten([1, [2, 3], 4]) should return [1, 2, 3, 4].
+
  flatten([5, [4, [3, 2]], 1]) should return [5, 4, 3, 2, 1].
+
  flatten(["A", [[[["B"]]]], "C"]) should return ["A", "B", "C"].
+
  flatten([["L", "M", "N"], ["O", ["P", "Q", ["R", ["S", ["T", "U"]]]]], "V", ["W", ["X", ["Y", ["Z"]]]]]) 
  should return ["L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"].
+ 
  flatten([["red", ["blue", ["green", ["yellow", ["purple"]]]]], "orange", ["pink", ["brown"]]]) 
  should return ["red","blue","green","yellow","purple","orange","pink","brown"].
 
