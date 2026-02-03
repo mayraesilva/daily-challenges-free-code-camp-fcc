f2c5b625ded8823386bb0b68ec027a0d7e77ae61
Mayra Silva <mayraeduarda2002@gmail.com>
2026-02-03 15:00:52 -0300

    intructions february 3 chall 5

---
+++ 2026-02-03-challenge-fcc.py
@@ -0,0 +1,17 @@
+"""
+Hi! My name is Mayra Silva and I'm studying to become a data scientist.
+Today I'll be solving the daily challenge available on 3rd, February, 2026.
+
+----------------------------
+String Mirror
+
+Given a string, return a new string that consists of the given string 
+with a reversed copy of itself appended to the end of it.
+
+---------------------------
+Tests
+1. mirror("freeCodeCamp") should return "freeCodeCamppmaCedoCeerf".
+2. mirror("RaceCar") should return "RaceCarraCecaR".
+3. mirror("helloworld") should return "helloworlddlrowolleh".
+4. mirror("The quick brown fox...") should return "The quick brown fox......xof nworb kciuq ehT".
+"""
