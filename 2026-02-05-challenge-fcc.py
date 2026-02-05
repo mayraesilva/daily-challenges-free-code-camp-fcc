22e6e626330f8392aac16c72c727ac043d454147
Mayra Silva <mayraeduarda2002@gmail.com>
2026-02-05 15:58:12 -0300

    chall 7 - firts version

--- 2026-02-05-challenge-fcc.py
+++ 2026-02-05-challenge-fcc.py
@@ -20,3 +20,54 @@ Tests
 5. count_change([1]) should return "$0.01".
 6. count_change([25, 25, 25, 25]) should return "$1.00".
 """
+
+def count_change(change):
+    total_cents = 0
+    value = ''
+
+    for coin in change: #calculating the total in cents
+        total_cents += coin
+    
+    if total_cents < 10:
+        value = f'$0.0{total_cents}'
+        return value
+
+    if total_cents < 100:
+        
+        value = f'$0.{total_cents}'
+        print(value)
+        return value
+    
+    else:
+
+        dollars = total_cents // 100
+        cents = total_cents % 100
+        cents_string = f'{cents}'
+
+        if cents == 0: 
+
+            value = f'${dollars}.00'
+            print(value)
+            return value
+        
+        elif len(cents_string) == 1:
+
+            value = f'${dollars}.0{cents}'
+            print(value)
+            return value
+        
+        else:
+
+            value = f'${dollars}.{cents}'
+            print(value)
+            return value
+
+
+
+
+count_change([25, 10, 5, 1])
+count_change([25, 10, 5, 1, 25, 10, 25, 1, 1, 10, 5, 25])
+count_change([100, 25, 100, 1000, 5, 500, 2000, 25])
+count_change([10, 5, 1, 10, 1, 25, 1, 1, 5, 1, 10])
+count_change([1])
+count_change([25, 25, 25, 25])
