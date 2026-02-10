31bb7b457bc69607a2ae6a21a765eed5cb760fec
Mayra Silva <mayraeduarda2002@gmail.com>
2026-02-10 11:10:36 -0300

    chall 12 - converting time

--- 2026-02-10-challenge-fcc.py
+++ 2026-02-10-challenge-fcc.py
@@ -30,8 +30,20 @@ should return ["0", "+0:02", "+1:01", "+2:17", "+2:45", "+3:03", "+3:59", "+4:18
  should return ["0", "+9:30", "+9:38", "+9:49", "+10:40", "+12:12", "+13:15", "+13:55"].
 """
 
+from datetime import datetime 
 
 def get_relative_results(results):
+
+    date_format = '%H:%M:%S'
+    results_new_format = []
+
+    for result in results:
+        new_value = datetime.strptime(date_format, result)
+        results_new_format.append(new_value)
+    
+    print(results_new_format)
+
+
 
     return results
 
