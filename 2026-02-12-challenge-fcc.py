0dad2378cada1934d0081518d4abab00ef4acd93
Mayra Silva <mayraeduarda2002@gmail.com>
2026-02-12 15:46:17 -0300

    chall 14 - enumerate corrrection

--- 2026-02-12-challenge-fcc.py
+++ 2026-02-12-challenge-fcc.py
@@ -26,10 +26,20 @@ def largest_difference(skater1, skater2):
     largest_gap = 0
     differences = []
 
-    for index, lap in skater1:
+    for index, lap in enumerate(skater1):
         difference = abs(skater1[index] - skater2[index])
         differences.append(difference)                 
 
     print(differences)
 
     return largest_gap
+
+
+
+#tests
+
+largest_difference([26.11, 25.80, 25.92, 26.23, 26.07], [25.93, 25.74, 26.53, 26.11, 26.30])
+largest_difference([27.04, 25.94, 26.22, 26.07, 26.18], [25.59, 25.80, 26.11, 26.01, 26.23])
+largest_difference([25.82, 25.90, 26.05, 26.00, 26.48], [25.85, 25.92, 26.07, 25.98, 25.95])
+largest_difference([25.88, 26.10, 25.95, 26.05, 26.00], [25.90, 26.55, 25.92, 26.03, 26.01])
+largest_difference([25.92, 26.01, 26.05, 25.88, 26.12], [25.95, 26.00, 26.03, 26.45, 26.10])
