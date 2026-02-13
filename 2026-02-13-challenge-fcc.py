d20080f09da65757bfccbd892bde51ae7c4b1909
mayraesilva <mayraeduarda2002@gmail.com>
2026-02-13 12:47:10 -0300

    chall 15 - calculating speed

--- 2026-02-13-challenge-fcc.py
+++ 2026-02-13-challenge-fcc.py
@@ -41,22 +41,38 @@ should return "The luger's fastest speed was 35.07 m/s on segment 5."
 
 4. get_fastest_speed([8.490, 7.732, 10.103, 8.489, 6.840])
  should return "The luger's fastest speed was 37.69 m/s on segment 1."
- 
+
 5. get_fastest_speed([8.204, 7.230, 9.673, 7.645, 6.508])
  should return "The luger's fastest speed was 39.24 m/s on segment 4."
 """
 
 
 def get_fastest_speed(times):
+    segments = {"segment 1": 320,
+                "segment 2": 280,
+                "segment 3": 350,
+                "segment 4": 300,
+                "segment 5": 250}
+    
+    segment_speed = []
+
+    for index, (key, value) in enumerate(segments.items()):
+        speed = value / times[index]
+        segment_speed.append(speed)
+    
+    print(segment_speed)
+        
+    
+
 
     return times
 
 
 #Tests
 get_fastest_speed([9.523, 8.234, 10.012, 9.001, 7.128])
-get_fastest_speed([9.381, 7.417, 9.912, 8.815, 7.284])
-get_fastest_speed([8.890, 7.601, 9.093, 8.392, 6.912])
-get_fastest_speed([8.490, 7.732, 10.103, 8.489, 6.840])
-get_fastest_speed([8.204, 7.230, 9.673, 7.645, 6.508])
+# get_fastest_speed([9.381, 7.417, 9.912, 8.815, 7.284])
+# get_fastest_speed([8.890, 7.601, 9.093, 8.392, 6.912])
+# get_fastest_speed([8.490, 7.732, 10.103, 8.489, 6.840])
+# get_fastest_speed([8.204, 7.230, 9.673, 7.645, 6.508])
 
 
