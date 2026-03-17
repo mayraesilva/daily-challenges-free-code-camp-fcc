df50c44910f0a1bb5f831f78375c475e328cdd75
Mayra Silva <mayraeduarda2002@gmail.com>
2026-03-17 12:10:18 -0300

    march17 - instructions

---
+++ 2026-03-17-challenge-fcc.py
@@ -0,0 +1,48 @@
+"""
+Hi! My name is Mayra Silva and I'm studying to become a data scientist.
+Today I'll be solving the daily challenge available on 17th, March, 2026.
+
+----------------------------
+Anniversary Milestones
+Given an integer representing the number of years a couple has been married, 
+return their most recent anniversary milestone according to this chart:
+
+Years Married	Milestone
+1	"Paper"
+5	"Wood"
+10	"Tin"
+25	"Silver"
+40	"Ruby"
+50	"Gold"
+60	"Diamond"
+70	"Platinum"
+
+If they haven't reached the first milestone, return "Newlyweds".
+-----------------------------
+Tests:
+1. get_milestone(0) should return "Newlyweds".
+2. get_milestone(1) should return "Paper".
+3. get_milestone(8) should return "Wood".
+4. get_milestone(10) should return "Tin".
+5. get_milestone(26) should return "Silver".
+6. get_milestone(45) should return "Ruby".
+7. get_milestone(50) should return "Gold".
+8. get_milestone(64) should return "Diamond".
+9. get_milestone(71) should return "Platinum".
+"""
+
+def get_milestone(years):
+
+    return years
+
+# Tests
+
+get_milestone(0) 
+get_milestone(1) 
+get_milestone(8) 
+get_milestone(10)
+get_milestone(26)
+get_milestone(45)
+get_milestone(50)
+get_milestone(64)
+get_milestone(71)
