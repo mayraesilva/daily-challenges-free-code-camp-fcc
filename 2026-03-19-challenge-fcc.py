8e92f2474a738a1818eaa674d5f1e2f98b7885b4
Mayra Silva <mayraeduarda2002@gmail.com>
2026-03-19 09:09:57 -0300

    Working on loops to identify first and second values, trying to swap places

--- 2026-03-19-challenge-fcc.py
+++ 2026-03-19-challenge-fcc.py
@@ -14,6 +14,7 @@ For example, given:
   ["a", "b"],
   ["a", "a"]
 ]
+
 Return:
 
 [
@@ -40,11 +41,55 @@ should return [[2.1, 1.2, 1.2, 1.2], [1.2, 2.1, 1.2, 2.1], [2.1, 2.1, 1.2, 1.2]]
 
 """
 
+def invert_matrix(matrix):
+
+    first_value = ''
+    second_value = ''
+    values_obtained = False # We still don't have both values
+
+    for line in matrix: 
+
+        for element in line:
+            if first_value == '': # Find the first value
+                first_value = element
+                print(first_value)
+                continue
+
+            if element == first_value: # Check if the next one is equal
+                #print('teste')
+                continue
+
+            else:
+                second_value = element # Get second value
+                values_obtained = True
+                print(second_value)
+                break
+
+        if values_obtained:
+            break
+
+    
+    new_matrix = []
+
+    for line in matrix:
+        new_line = []
+        for element in line:
+            if element == first_value:
+                new_line.append(second_value) # swap places
+            else:
+                new_line.append(first_value) # swap places
+
+            new_matrix.append(new_line) # Add new line
+    
+    print(new_matrix)
+
+
+    return new_matrix
 
 
 #Tests
 invert_matrix([["a", "b"], ["a", "a"]])
-invert_matrix([[1, 0, 1], [1, 1, 1], [0, 1, 0]])
-invert_matrix([["apple", "banana", "banana", "apple"], ["banana", "apple", "apple", "banana"], ["banana", "banana", "banana", "apple"]])
-invert_matrix([[6, 7, 7, 7, 6], [7, 6, 7, 6, 7], [7, 7, 6, 7, 7], [7, 6, 7, 6, 7], [6, 7, 7, 7, 6]])
-invert_matrix([[1.2, 2.1, 2.1, 2.1], [2.1, 1.2, 2.1, 1.2], [1.2, 1.2, 2.1, 2.1]])
+#invert_matrix([[1, 0, 1], [1, 1, 1], [0, 1, 0]])
+#invert_matrix([["apple", "banana", "banana", "apple"], ["banana", "apple", "apple", "banana"], ["banana", "banana", "banana", "apple"]])
+#invert_matrix([[6, 7, 7, 7, 6], [7, 6, 7, 6, 7], [7, 7, 6, 7, 7], [7, 6, 7, 6, 7], [6, 7, 7, 7, 6]])
+#invert_matrix([[1.2, 2.1, 2.1, 2.1], [2.1, 1.2, 2.1, 1.2], [1.2, 1.2, 2.1, 2.1]])
