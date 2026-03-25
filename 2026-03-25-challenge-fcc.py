a6c3657a0fdf6f0c82a9e1360380ed14ad5b4933
Mayra Silva <mayraeduarda2002@gmail.com>
2026-03-25 14:07:12 -0300

    march 25 instructions

---
+++ 2026-03-25-challenge-fcc.py
@@ -0,0 +1,34 @@
+"""
+Hi! My name is Mayra Silva and I'm studying to become a data scientist.
+Today I'll be solving the daily challenge available on 25th, March, 2026.
+
+----------------------------
+Cooldown Time
+
+Given two timestamps, the first representing when a user finished an exam, 
+and the second representing the current time, 
+determine whether the user can take an exam again.
+
+Both timestamps will be given the format:
+ "YYYY-MM-DDTHH:MM:SS", for example "2026-03-25T14:00:00".
+   Note that the time is 24-hour clock.
+A user must wait at least 48 hours before retaking an exam.
+----------------------------
+
+Tests:
+
+1. can_retake("2026-03-23T08:00:00", "2026-03-25T14:00:00") should return True.
+2. can_retake("2026-03-24T14:00:00", "2026-03-25T10:00:00") should return False.
+3. can_retake("2026-03-23T09:25:00", "2026-03-25T09:25:00") should return True.
+4. can_retake("2026-03-25T11:50:00", "2026-03-23T11:49:59") should return False.
+"""
+def can_retake(finish_time, current_time):
+
+    return finish_time
+
+
+#Tests
+can_retake("2026-03-23T08:00:00", "2026-03-25T14:00:00")
+can_retake("2026-03-24T14:00:00", "2026-03-25T10:00:00")
+can_retake("2026-03-23T09:25:00", "2026-03-25T09:25:00")
+can_retake("2026-03-25T11:50:00", "2026-03-23T11:49:59")
