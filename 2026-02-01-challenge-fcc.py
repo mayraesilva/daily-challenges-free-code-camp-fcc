85e6b7a577ec45bf3817d20a70643a6bf3612643
mayraesilva <mayraeduarda2002@gmail.com>
2026-05-01 21:28:16 -0300

    feb 1st - done

--- 2026-02-01-challenge-fcc.py
+++ 2026-02-01-challenge-fcc.py
@@ -32,53 +32,63 @@ from datetime import datetime, timedelta
 
 def digital_detox(logs):
 
-    time_log_format = "%Y-%m-%d %H:%M:%S"
+    time_log_format = "%Y-%m-%d %H:%M:%S" 
     logs_datetime = []
     status = None #The status can be true or false, based on the conditions
     how_many_logs_registered = len(logs)
 
+    # Make sure we have the logs in ascending order
+    logs.sort()
+    
     # Step 1: Make sure the data type is ready
 
     for log_string in logs:
         log_datetime = datetime.strptime(log_string, time_log_format)
         logs_datetime.append(log_datetime)
 
-    # Step 2: Check if in the same day we have more than one login  a day
+
+    # Variables we need
     minimum_time_off = timedelta(hours=4) #minimum hours to be offline before checking socials a again
-    logs_a_day = 0
+    logs_a_day = 1
     max_logs_a_day = 2 
+
+    # Step 2: Check if in the same day we have more than one login  a day
 
     for index, log in enumerate(logs_datetime):
 
         todays_log = log
 
+        # make sure we are comparing to the next log
         if index + 1 < how_many_logs_registered:
             next_log = logs_datetime[index + 1]
 
         else:
-            next_log = todays_log
-
-        hours_difference = next_log - todays_log
-
-        if todays_log.day == next_log.day:
+            break #This log is the last one
             
+
+        # How many time between logs?
+        hours_difference = next_log - todays_log
+        #print(hours_difference)
+
+        # Step 3: Check the intervals between logs
+
+        if hours_difference < minimum_time_off: # !!!!!!!!!!!!!!!!!!!!!!!!!!
+            print("You should give more space between logs")
+            status = False
+            return status 
+            
+        # SAME DAY
+        if todays_log.day == next_log.day: # Is it the same day?
+            logs_a_day += 1
+            #print(logs_a_day)
+          
             if todays_log == next_log: # we reached the last log
                 break
-
-            else:
-                logs_a_day += 1
-
+                
             if logs_a_day > max_logs_a_day:
                 print("You should reduce the logs per day")
                 status = False
                 return status
-            
-            # Step 3: Considering we have same day logs, check the intervals between logs
-
-            elif hours_difference < minimum_time_off:
-                print("You should give more space between logs")
-                status = False
-                return status 
             
         else:
             logs_a_day = 0
@@ -87,29 +97,7 @@ def digital_detox(logs):
     print('You are on the right track!')
     status = True
     return status
-            
-    
-    
-
-            
-
-
         
-
-        
-
-            
-        
-
-
-
-
-
-
-
-
-    return logs
-
 
 # Tests
 
