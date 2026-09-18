5e7172c85584976fde6b46b4963cdb45cc142573
mayraesilva <mayraeduarda2002@gmail.com>
2026-09-18 14:10:11 -0300

    septermber 18th, instructions

---
+++ 2026-09-18-challenge-fcc.py
@@ -0,0 +1,37 @@
+"""
+Hi! My name is Mayra Silva and I'm studying to become a data scientist.
+Today I'll be solving the daily challenge available on September, 18th, 2026.
+
+----------------------------
+Fill The Tank
+
+Given the size of a fuel tank, the current fuel level, and the price per gallon, 
+return the cost to fill the tank all the way.
+
+- tankSize is the total capacity of the tank in gallons.
+- fuelLevel is the current amount of fuel in the tank in gallons.
+- pricePerGallon is the cost of one gallon of fuel.
+
+The returned value should be rounded to two decimal places in the format: "$d.dd".
+
+------
+Tests:
+1. cost_to_fill(20, 0, 4.00) should return "$80.00".
+2. cost_to_fill(15, 10, 3.50) should return "$17.50".
+3. cost_to_fill(18, 9, 3.25) should return "$29.25".
+4. cost_to_fill(12, 12, 4.99) should return "$0.00".
+5. cost_to_fill(15, 9.5, 3.98) should return "$21.89".
+"""
+
+def cost_to_fill(tank_size, fuel_level, price_per_gallon):
+
+    return tank_size
+
+
+
+
+cost_to_fill(20, 0, 4.00)
+cost_to_fill(15, 10, 3.50)
+cost_to_fill(18, 9, 3.25)
+cost_to_fill(12, 12, 4.99)
+cost_to_fill(15, 9.5, 3.98)
